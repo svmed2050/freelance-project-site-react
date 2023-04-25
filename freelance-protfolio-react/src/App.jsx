5debfbd5843339dfb0f7abd27e37f99eb151c3a9
Pavel Sergienko <pavelsergienko7@gmail.com>
2023-04-25 17:39:51 +0300

    add project page and home page

--- freelance-protfolio-react/src/App.jsx
+++ freelance-protfolio-react/src/App.jsx
@@ -1,35 +1,17 @@
 import './styles/main.css'
 
 import Navbar from './components/navbar/Navbar'
-import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
 
 function App() {
 	return (
 		<div className='App'>
 			<Navbar />
 
-			<Header />
-
-			<main className='section '>
-				<div className='container'>
-					<h2 className='title-1'>Skills</h2>
-					<ul className='content-list'>
-						<li className='content-list__item'>
-							<h2 className='title-2'>Frontend</h2>
-							<p>
-								{' '}
-								JavaScript, TypeScript, ReactJS, Redux, HTML, CSS, NPM,
-								BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents
-							</p>
-						</li>
-						<li className='content-list__item'>
-							<h2 className='title-2'>Backend</h2>
-							<p>NodeJS, MySQL, MongoDB</p>
-						</li>
-					</ul>
-				</div>
-			</main>
+			{/* <Home /> */}
+			<Projects />
 
 			<Footer />
 		</div>
